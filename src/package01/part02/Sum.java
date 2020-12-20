5e7c6e0f74fc4a296509176bb391f7a84250b65b
youngjinchoi916 <defectio916@gmail.com>
2020-12-20 15:45:48 +0900

    세번째 커밋 : 1~20까지의 합으로 변경

--- src/package01/part02/Sum.java
+++ src/package01/part02/Sum.java
@@ -3,10 +3,10 @@ package package01.part02;
 public class Sum {
 
 	public static void main(String[] args) {
-		System.out.println("[1~10까지의 합 구하기]");
+		System.out.println("[1~20까지의 합 구하기]");
 		int sum = 0;
 		
-		for(int i=0 ; i<11 ; i++) {
+		for(int i=0 ; i<21 ; i++) {
 			sum += i;
 		}
 		System.out.println("sum : "+sum);
