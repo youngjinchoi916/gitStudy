2cb7a47c1b761289f5c29b04321c3f854a8d6a54
youngjinchoi916 <defectio916@gmail.com>
2020-12-22 14:06:02 +0900

    구구단 2~4단 출력

---
+++ src/package01/part02/Multiple2.java
@@ -0,0 +1,16 @@
+package package01.part02;
+
+public class Multiple2 {
+
+	public static void main(String[] args) {
+		System.out.println("[구구단 4단 출력]");
+		
+		int dan = 4;
+		
+		for(int i=1 ; i<=9 ; i++) {
+			System.out.println("4 * "+i+" = "+dan*i);
+		}
+
+	}
+
+}
---
+++ src/package01/part02/Multiple4.java
@@ -0,0 +1,16 @@
+package package01.part02;
+
+public class Multiple4 {
+
+	public static void main(String[] args) {
+		System.out.println("[구구단 2단 출력]");
+		
+		int dan = 2;
+		
+		for(int i=1 ; i<=9 ; i++) {
+			System.out.println("2 * "+i+" = "+dan*i);
+		}
+
+	}
+
+}
